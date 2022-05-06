b82a590fb25797061daa7780a955f2bd1217917a
Dongwon Kim <dongwon000103@gmail.com>
2022-05-06 16:48:18 +0000

    copy repo

---
+++ public/build/precache-manifest.7b5223b937d55ac5af1ddb18662441a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f72c6e722bb47536dd53adc7c699a08e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "56d5052a7128f97383df",
+    "url": "./static/css/main.1fb77d74.chunk.css"
+  },
+  {
+    "revision": "6ddae469c4cea4a33ada",
+    "url": "./static/js/2.d90b917e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.d90b917e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56d5052a7128f97383df",
+    "url": "./static/js/main.0b034900.chunk.js"
+  },
+  {
+    "revision": "b83a8905763b949033c2",
+    "url": "./static/js/runtime-main.67a12c9c.js"
+  }
+]);
---
+++ public/build/precache-manifest.fb8fdae7b4ac666c655e0f5b08dfb19e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ac64f0547e8edf2c7dc0dc71a0fd13b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f76581f055122ca3a019",
+    "url": "./static/css/main.2120618e.chunk.css"
+  },
+  {
+    "revision": "0d2ceed4f7377fd8298f",
+    "url": "./static/js/2.efa375dc.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.efa375dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f76581f055122ca3a019",
+    "url": "./static/js/main.3a6116a6.chunk.js"
+  },
+  {
+    "revision": "b83a8905763b949033c2",
+    "url": "./static/js/runtime-main.67a12c9c.js"
+  }
+]);
